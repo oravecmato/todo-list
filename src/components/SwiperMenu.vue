2624f5e7173ccfc18b6e4fdda4b31357f02aaa9c
oravecmato <oravec.mato@gmail.com>
2023-05-14 23:12:48 +0200

    Swiper menu prototype in progress

--- src/components/SwiperMenu.vue
+++ src/components/SwiperMenu.vue
@@ -10,14 +10,18 @@ const opacity = ref(0)
 
 const menuShown = ref(false);
 
-function reset() {
-  translateX.value = `-${sidebarWidth.value}px`
-  opacity.value = 0
-  menuShown.value = false;
+function resetSidebarStyles(expanded: boolean) {
+  if (expanded) {
+    translateX.value = '0'
+    opacity.value = 1
+  } else {
+    translateX.value = `-${sidebarWidth.value}px`
+    opacity.value = 0
+  }
 }
 
 onMounted(() => {
-  reset();
+  resetSidebarStyles(false);
 })
 
 const { direction, isSwiping, lengthX, lengthY } = useSwipe(
@@ -26,30 +30,48 @@ const { direction, isSwiping, lengthX, lengthY } = useSwipe(
       onSwipe(e: TouchEvent) {
         if (sidebarWidth.value) {
           if (lengthX.value < 0) {
-            const length = Math.min(Math.abs(lengthX.value), sidebarWidth.value);
-            translateX.value = `-${sidebarWidth.value - length}px`;
-            opacity.value = Math.min(0.1 + length / sidebarWidth.value, 1);
+            if (!menuShown.value) {
+              const length = Math.min(Math.abs(lengthX.value), sidebarWidth.value);
+              translateX.value = `-${sidebarWidth.value - length}px`;
+              opacity.value = Math.min(0.1 + length / sidebarWidth.value, 1);
+            }
           }
           else {
-            // reset();
+            if (menuShown.value) {
+              const length = Math.min(lengthX.value, sidebarWidth.value);
+              translateX.value = `-${length}px`;
+              opacity.value = Math.min(1.1 - length / sidebarWidth.value, 1);
+            }
+
           }
         }
       },
       onSwipeEnd(e: TouchEvent) {
-        if (lengthX.value < 0 && sidebarWidth.value && (Math.abs(lengthX.value) / sidebarWidth.value) >= 0.5) {
-          translateX.value = '0'
-          opacity.value = 1
-          menuShown.value = true;
-        }
-        else {
-          reset();
+        if (!menuShown.value) {
+          if (lengthX.value < 0 && sidebarWidth.value && (Math.abs(lengthX.value) / sidebarWidth.value) >= 0.5) {
+            resetSidebarStyles(true);
+            menuShown.value = true;
+          }
+          else {
+            resetSidebarStyles(false);
+            menuShown.value = false;
+          }
+        } else {
+          if (lengthX.value > 0 && sidebarWidth.value && (lengthX.value / sidebarWidth.value) >= 0.5) {
+            resetSidebarStyles(false);
+            menuShown.value = false;
+          }
+          else {
+            resetSidebarStyles(true);
+            menuShown.value = true;
+          }
         }
       },
     })
 </script>
 
 <template>
-  <div style="width: 100vw; min-height: 100vh;" ref="target">
+  <div style="width: 100%; min-height: 100vh;" ref="target">
     <div class="container select-none">
       <button @click="reset">
         Reset

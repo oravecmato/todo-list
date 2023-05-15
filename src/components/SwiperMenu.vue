5001815b4ded8bf05a4069ec927ecec3e96a1ca9
oravecmato <oravec.mato@gmail.com>
2023-05-15 04:40:32 +0200

    custom library tests version upd

--- src/components/SwiperMenu.vue
+++ src/components/SwiperMenu.vue
@@ -27,7 +27,7 @@ const assignSidebarWidthValue = () => {
 
 onMounted(() => {
   resetSidebarStyles(false);
-  alert('Version 2.3')
+  alert('Version 2.4')
 })
 
 const dynamicStyles = computed(() => sidebarShown.value && isSwiping.value ? { transform: `translateX(${translateX.value})`, opacity: opacity.value } : {});
